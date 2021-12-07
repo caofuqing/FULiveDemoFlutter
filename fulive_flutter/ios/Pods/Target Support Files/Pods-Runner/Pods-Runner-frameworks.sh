a1fb70bc00c73eea6bd5cf9c66002f9f9ffcfbde
caofuqing <caofuqing@yxtreme.cn>
2021-12-07 14:58:29 +0800

    add ios run

--- fulive_flutter/ios/Pods/Target Support Files/Pods-Runner/Pods-Runner-frameworks.sh
+++ fulive_flutter/ios/Pods/Target Support Files/Pods-Runner/Pods-Runner-frameworks.sh
@@ -113,6 +113,7 @@ install_dsym() {
       rsync --delete -av "${RSYNC_PROTECT_TMP_FILES[@]}" --links --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${DERIVED_FILES_DIR}/${basename}.dSYM" "${DWARF_DSYM_FOLDER_PATH}"
     else
       # The dSYM was not stripped at all, in this case touch a fake folder so the input/output paths from Xcode do not reexecute this script because the file is missing.
+      mkdir -p "${DWARF_DSYM_FOLDER_PATH}"
       touch "${DWARF_DSYM_FOLDER_PATH}/${basename}.dSYM"
     fi
   fi
@@ -175,21 +176,21 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/FURenderKit/FURenderKit-v7.4.1/FURenderKit.framework"
+  install_framework "${PODS_ROOT}/FURenderKit/FURenderKit-7.4.1/FURenderKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toast/Toast.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/fluttertoast/fluttertoast.framework"
 fi
 if [[ "$CONFIGURATION" == "Profile" ]]; then
-  install_framework "${PODS_ROOT}/FURenderKit/FURenderKit-v7.4.1/FURenderKit.framework"
+  install_framework "${PODS_ROOT}/FURenderKit/FURenderKit-7.4.1/FURenderKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toast/Toast.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/fluttertoast/fluttertoast.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/FURenderKit/FURenderKit-v7.4.1/FURenderKit.framework"
+  install_framework "${PODS_ROOT}/FURenderKit/FURenderKit-7.4.1/FURenderKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toast/Toast.framework"
